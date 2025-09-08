e382b2989a179f12a4cd14d47124dc982983cb00
Kim Tao <kim.tao@mongodb.com>
2025-09-08 13:56:00 -0400

    Bump maven version to latest

--- scripts/generate-openapi-client.sh
+++ scripts/generate-openapi-client.sh
@@ -5,9 +5,10 @@ set -eo pipefail
 mkdir -p bin
 cd bin
 
+maven_version=3.9.11
 # Download maven (required by OpenAPI generator CLI).
-curl -O https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
-tar xvzf apache-maven-3.9.9-bin.tar.gz
+curl -O "https://dlcdn.apache.org/maven/maven-3/${maven_version}/binaries/apache-maven-${maven_version}-bin.tar.gz"
+tar xvzf "apache-maven-${maven_version}-bin.tar.gz"
 
 # Download OpenAPI generator CLI.
 curl -O https://raw.githubusercontent.com/OpenAPITools/openapi-generator/master/bin/utils/openapi-generator-cli.sh
@@ -16,7 +17,7 @@ chmod +x ./openapi-generator-cli.sh
 cd ..
 
 # Generate OpenAPI client.
-export PATH="${PWD}"/apache-maven-3.9.9/bin:"${PATH}"
+export PATH="${PWD}/apache-maven-${maven_version}/bin":"${PATH}"
 export PATH=/opt/bin/java/jdk21/bin:"${PATH}"
 # It's assumed that the latest JSON spec is downloaded locally in bin/test_selection_services.json.
 bin/openapi-generator-cli.sh generate -i bin/test_selection_services.json -g go -o ./ --git-user-id evergreen-ci --git-repo-id test-selection-client
